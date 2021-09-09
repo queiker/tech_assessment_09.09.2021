c37199fad36035bcb96625ef4e57f7dbbf8bd74e
Grzegorz Płonka Kraków ul.Źródlana 11 30-734 <tradecomp.pl@gmail.com>
2021-09-09 09:08:11 +0200

    Project begin

---
+++ LifeOfAnts/LifeOfAnts/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LifeOfAnts
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int colonyWidth = 21;
+
+        }
+    }
+}
